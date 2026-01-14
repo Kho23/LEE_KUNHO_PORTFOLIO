0f87a16800fc10eefe32fe84fbf9044f960fe91b
Kho23 <wndrn2002@naver.com>
2026-01-14 16:20:42 +0900

    sh yml 수정중

--- deploy.sh
+++ deploy.sh
@@ -18,20 +18,21 @@ echo "Moving to app directory..."
 cd $APP_DIR || exit 1
 
 echo "Starting Backend Server..."
+# 환경변수 주입 방식을 명확히 개선
 nohup java -jar $JAR_NAME \
   --spring.profiles.active=prod \
-  --spring.datasource.url="$RDS_URL" \
-  --spring.datasource.username="$RDS_USERNAME" \
-  --spring.datasource.password="$RDS_PASSWORD" \
+  --spring.datasource.url="${RDS_URL}" \
+  --spring.datasource.username="${RDS_USERNAME}" \
+  --spring.datasource.password="${RDS_PASSWORD}" \
   --spring.jpa.hibernate.ddl-auto=update \
-  --Google="$GOOGLE_KEY" \
-  --JWT_SECRET="$JWT_SECRET" \
-  --KAKAO_CLIENT_ID="$KAKAO_CLIENT_ID" \
-  --KAKAO_CLIENT_SECRET="$KAKAO_CLIENT_SECRET" \
-  --NAVER_CLIENT_ID="$NAVER_CLIENT_ID" \
-  --NAVER_CLIENT_SECRET="$NAVER_CLIENT_SECRET" \
-  --iamport.api.key="$IAMPORT_API_KEY" \
-  --iamport.api.secret="$IAMPORT_API_SECRET" \
+  --Google="${GOOGLE_KEY}" \
+  --JWT_SECRET="${JWT_SECRET}" \
+  --KAKAO_CLIENT_ID="${KAKAO_CLIENT_ID}" \
+  --KAKAO_CLIENT_SECRET="${KAKAO_CLIENT_SECRET}" \
+  --NAVER_CLIENT_ID="${NAVER_CLIENT_ID}" \
+  --NAVER_CLIENT_SECRET="${NAVER_CLIENT_SECRET}" \
+  --iamport.api.key="${IAMPORT_API_KEY}" \
+  --iamport.api.secret="${IAMPORT_API_SECRET}" \
   > nohup.out 2>&1 &
 
 echo "Deployment finished."
